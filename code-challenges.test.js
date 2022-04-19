b59fb32f461120e43e70286d429cea250ec4c599
Sarah Proctor <sproctor950@gmail.com>
2022-04-19 10:05:50 -0700

    adds jest dependencies and first code challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,15 +10,37 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+// describe("hello", () => {
+//   it("returns a string that says hi", () => {
+//     expect(hello()).toEqual("hi")
+//   })
+// })
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
 // a) Create a test with an expect statement using the variable provided. HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+describe("contentShuffler", () => {
+  it("takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
+    const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+    // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+    const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+    // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+    expect(contentShuffler(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+    expect(contentShuffler(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+  })
+})
+
+// ReferenceError: contentShuffler is not defined
+
+// function - contentShuffler
+// parameter - array
+// slice first index
+// array.sort(() => Math.random() - 0.5) - Math.random() creates the random number, by subtracting .5 we can get a negative number or a positive number randomly, sort will treat negative and positive numbers differently
+
+const contentShuffler = (array) => {
+  return array.slice(1).sort(() => Math.random() - 0.5)
+}
 
 
 // b) Create the function that makes the test pass.
