474f1dbfefa205f8ec15040c162b335067ac8941
Sarah Proctor <sproctor950@gmail.com>
2022-04-19 10:27:05 -0700

    adds tests for challenge two and three and functions for challenge two

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -50,22 +50,47 @@ const contentShuffler = (array) => {
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const nums1 = [3, 56, 90, -8, 0, 23, 6]
-// Expected output: [-8, 90]
-const nums2 = [109, 5, 9, 67, 8, 24]
-// Expected output: [5, 109]
+describe("minAndMax", () => {
+  it("takes an array of numbers and returns an array of the minimum and maximum numbers in that order", () => {
+    const nums1 = [3, 56, 90, -8, 0, 23, 6]
+    // Expected output: [-8, 90]
+    const nums2 = [109, 5, 9, 67, 8, 24]
+    // Expected output: [5, 109]
+    expect(minAndMax(nums1)).toEqual([-8, 90])
+    expect(minAndMax(nums2)).toEqual([5, 109])
+  })
+})
 
+// ReferenceError: minAndMax is not defined
+
+// function - minAndMax
+// parameter - array
+// Math.max and Math.min and pass all the values in the array with a spread operator
 
 // b) Create the function that makes the test pass.
+
+const minAndMax = (array) => {
+  return [Math.min(...array), Math.max(...array)]
+}
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const testArray1 = [3, 7, 10, 5, 4, 3, 3]
-const testArray2 = [7, 8, 2, 3, 1, 5, 4]
-// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+describe("removeDuplicates", () => {
+  it("that takes in two arrays as arguments and returns one array with no duplicate values.", () => {
+    const testArray1 = [3, 7, 10, 5, 4, 3, 3]
+    const testArray2 = [7, 8, 2, 3, 1, 5, 4]
+    // Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+    expect(removeDuplicates(testArray1, testArray2)).toEqual([3, 7, 10, 5, 4, 8, 2, 1])
+  })
+})
+
+// ReferenceError: removeDuplicates is not defined
+
+// function - removeDuplicates
+// parameter - array1, array2
 
 
 // b) Create the function that makes the test pass.
